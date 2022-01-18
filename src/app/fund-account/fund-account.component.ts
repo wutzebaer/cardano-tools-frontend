f674083a5c3caa5bedfed5dcc524db699fd7eafd
peter <petergrossmann@email.de>
2022-01-18 13:34:06 +0100

    show tx size

--- src/app/fund-account/fund-account.component.ts
+++ src/app/fund-account/fund-account.component.ts
@@ -41,6 +41,15 @@ export class FundAccountComponent implements OnInit, OnDestroy {
     });
   }
 
+  niceBytes(x: number) {
+    const units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    let l = 0, n = x || 0;
+    while (n >= 1024 && ++l) {
+      n = n / 1024;
+    }
+    return (n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l]);
+  }
+
   ngOnDestroy(): void {
     this.timer.unsubscribe();
     this.accountSubscription.unsubscribe();
@@ -49,7 +58,7 @@ export class FundAccountComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
   }
 
-  get invalid(){
+  get invalid() {
     return this.adaBalanceInput?.invalid || this.targetAddressInput?.invalid || this.txSizeInput?.invalid
   }
 
